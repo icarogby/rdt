6b8ce3e459dcbfe3e489d55213df24208ba830a3
Ícaro Gabryel <icarogabryel2001@gmail.com>
2023-03-07 13:30:50 -0300

    different ip

--- network.py
+++ network.py
@@ -4,11 +4,10 @@ from threading import Thread
 # todo fazer somente socket servidor
 host = socket.gethostbyname(socket.gethostname())
 port = 5000
+print(f"Host: {host} | Port: {port}")
 
-serv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # AF_INET = IPV4 | SOCK_DGRAM = UDP
-serv.bind((host, port))
-
-clie = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # AF_INET = IPV4 | SOCK_DGRAM = UDP
+skt.bind((host, port))
 
 opc = 1
 def menu():
@@ -25,19 +24,17 @@ def menu():
         opc = int(input("O que deseja fazer com o pacote: "))
 
 def baguncinha(opc, data, addr):
-    addr = (addr[0], 6000) #! Não mudar porta
+    global skt
 
     if opc == 1:
-        clie.sendto(data, addr)
+        skt.sendto(data, addr)
     elif opc == 2:
         pass
     elif opc == 3:
-        clie.sendto(data, addr)
-        print("dd1")
-        clie.sendto(data, addr)
-        print("dd2")
+        skt.sendto(data, addr)
+        skt.sendto(data, addr)
     elif opc == 4:
-        clie.sendto(data, addr)
+        skt.sendto(data, addr)
         # todo Fazer canal de volta
         # todo apagar
     elif opc == 5:
@@ -47,16 +44,16 @@ def baguncinha(opc, data, addr):
         print("Opção inválida")
     
 def gate():
-    global serv, clie
+    global skt
     global opc
 
     while True:
-        data, addr = serv.recvfrom(1024) # receive data and client address
+        data, addr = skt.recvfrom(1024) # receive data and client address
         print(addr)
 
         baguncinha(opc, data, addr)
 
-        print(f"Received data: {data.decode()}")
+        print(f": Received data: {data.decode()}")
 
 Thread(target=menu).start()
 Thread(target=gate).start()
