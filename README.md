d522cf74cd6df051d7858d80fbe1c6f45757345d
Ícaro Gabryel <91147230+icarogby@users.noreply.github.com>
2023-09-30 16:35:21 -0300

    sender complete

---
+++ README.md
@@ -0,0 +1,5 @@
+máquina de estados
+como implementar
+como o teste foi feito
+características: udp, envia um e espera ack, timeout, reenvia, etc
+| CHECKSUM 1 | CHECKSUM 2 | SIZE | SERIAL | DATA |
--- rdt.py
+++ rdt.py
@@ -1,36 +1,34 @@
-from socket import socket, AF_INET, SOCK_DGRAM, gethostbyname, gethostname
+from socket import socket, AF_INET, SOCK_DGRAM
+from threading import Thread
 from time import time
 
-# def receive():
-#     global skt, ack, listening
+# todo: add log
 
-#     while True:
-#         data, addr = skt.recvfrom(1024) # receive data and client address
-#         data = data.decode("utf-8")
-
-#         if not listening: #? Change here
-#             print("Received data. Ignoring...")
-#         else:
-#             if data == "ack0" and serial_number == 0: #? change here
-#                     print("Received right ack (0)")
-#                     ack = True
-#             elif data == "ack1" and serial_number == 1: #? change here
-#                 with critical:
-#                     print("Received right ack (1)")
-#                     ack = True
-#             else:
-#                 print("Received wrong ack. Ignoring...")
+TIME_OUT = 1
 
 class Sender():
-    def __init__(self, senderAddress: tuple, receiverAddress: tuple) -> None:
+    def __init__(self, senderAddress: tuple, receiverAddress: tuple, log = False):
         self.receiverAddress = receiverAddress
 
         self.serialNumber = 0
         self.isListening = False
-        self.ack = False
+        self.rightAck = False
 
         self.skt = socket(AF_INET, SOCK_DGRAM) # AF_INET = IPV4 | SOCK_DGRAM = UDP
         self.skt.bind(senderAddress)
+
+        self.thread = Thread(target=self.receive)
+        self.thread.start()
+
+    def receive(self):
+        while True:
+            receivedAck, _ = self.skt.recvfrom(1024)
+
+            if self.isListening:
+                if receivedAck == bytes([self.serialNumber]):
+                    self.rightAck = True
+                else:
+                    print("Received wrong ack. Ignoring...") # todo: take out
 
     def send(self, dataInBytes: bytes):
         for i in range(0, len(dataInBytes), 16):
@@ -38,9 +36,10 @@ class Sender():
             
             segmentedData = bytes([self.serialNumber]) + segmentedData
 
-            segmentSize = len(segmentedData)
+            segmentSize = len(segmentedData) + 2
             segmentedData = bytes([segmentSize]) + segmentedData
 
+            # Calculating checksum.
             sum = 0
 
             for byteInInt in segmentedData:
@@ -48,30 +47,63 @@ class Sender():
             
             checkSum = ~sum
 
-            checkSunIn2Bytes = bin(checkSum)[3:].zfill(16)
-            byte1 = checkSunIn2Bytes[0:8]
-            byte2 = checkSunIn2Bytes[8:16]
+            checkSunInTwoBytes = bin(checkSum)[3:].zfill(16)
+            checkSunByte1 = checkSunInTwoBytes[0:8]
+            checkSunByte2 = checkSunInTwoBytes[8:16]
             
-            segmentedData = bytes([int(byte1, 2), int(byte2, 2)]) + segmentedData
+            segmentedData = bytes([int(checkSunByte1, 2), int(checkSunByte2, 2)]) + segmentedData
 
+            # Sending segment.
             self.skt.sendto(segmentedData, self.receiverAddress)
             
-            # start = time()
-            # while True:
-            #     listening = True #? Change here
-                
-            #     end = time()
-            #     if end - start > 1:
-            #         print("Timeout! Resending segment...")
-            #         skt.sendto(segment.encode("utf-8"), (core_ip, 6000)) # todo: change to 5000 port
-            #         start = time()
-            #     else:
-            #         if ack:
-            #             ack = False
-            #             listening = False #? Change here
-            #             break
+            # Waiting for ack.
+            self.isListening = True
+            
+            start = time()
 
-            # if (serial_number == 0):
-            #     serial_number = 1
-            # else:
-            #     serial_number = 0
+            while True:
+                end = time()
+
+                if (end - start) > TIME_OUT:
+                    print("Timeout! Resending segment...") # todo: take out
+                    # Resending segment.
+                    self.skt.sendto(segmentedData, self.receiverAddress) # todo: change to 5000 port
+                    
+                    start = time()
+                else:
+                    if self.rightAck:
+                        print("Received ack. Moving on...") # todo: take out
+                        self.rightAck = False
+                        self.isListening = False
+
+                        break
+            
+            # Update serial number.
+            self.serialNumber = 1 - self.serialNumber
+
+class Reciever():
+    def __init__(self, receiverAddress: tuple):
+        self.ack = 0
+
+        self.skt = socket(AF_INET, SOCK_DGRAM) # AF_INET = IPV4 | SOCK_DGRAM = UDP
+        self.skt.bind(receiverAddress)
+
+    def receive(self):
+        buffer = b""
+
+        while True:
+            receivedData, _ = self.skt.recvfrom(1024)
+            receivedData = buffer + receivedData
+
+            try:
+                segmentSize = receivedData[2]
+                segmentData = receivedData[3:segmentSize]
+                buffer = receivedData[segmentSize:]
+            except:
+                print("Data receive is corrupted. ignoring\n") # todo: to log
+                continue
+            
+            try:
+                checkSumByte1 = receivedData[0]
+                checkSumByte2 = receivedData[1]
+                checkSum = int(bin(checkSumByte1)[2:].zfill(8) + bin(checkSumByte2)[2:].zfill(8), 2)
